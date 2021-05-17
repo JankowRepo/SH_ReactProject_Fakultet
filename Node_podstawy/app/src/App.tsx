d10809e27e86f9d232dd750deaf74950c0241092
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-05-17 18:18:30 +0200

    notatki wykład nr 5

--- Node_podstawy/app/src/App.tsx
+++ Node_podstawy/app/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-
+import {Provider} from "react-redux";
 import Movie from './views/movie/Movie';
 import Home from './views/home/Home';
 import Page404 from "./views/404page/Page404";
+import store from "./store";
 
 import './App.css';
 
@@ -11,11 +12,13 @@ const App=()=>{
     return (
         <div className={"App"}>
             <BrowserRouter>
-                <Switch>
-                    <Route path="/" component={Home} exact/>
-                    <Route path="/movie/:id" component={Movie} />
-                    <Route path="*" component={Page404} />
-                </Switch>
+                <Provider store={ store }>
+                    <Switch>
+                        <Route path="/movie/:id" component={Movie} />
+                        <Route path="/" component={Home} exact/>
+                        <Route path="*" component={Page404} />
+                    </Switch>
+                </Provider>
             </BrowserRouter>
         </div>
     );
---
+++ Node_podstawy/app/src/store/store.ts
@@ -0,0 +1,17 @@
+import {combineReducers} from "redux";
+import { TodosList,todosListInitialState, todosStoreReducer} from "./reducers/todos.reducer";
+import {Actions as TodoActions} from "./actions/todos.actions";
+
+export type StoreActionTypes=TodoActions;
+
+export interface StoreState{
+    todos: TodosList;
+};
+export const initialStoreState: any={
+    todos: todosListInitialState,
+};
+
+export const reducers=combineReducers<StoreState>({
+    todos: todosStoreReducer,
+});
+
